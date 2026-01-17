5936311740629ebbc2e735deb1e307b1e683d8b3
SADIQ-123-nitezen <ssd15437@gmail.com>
2026-01-17 17:21:23 +0530

    Added new payment methods in the get_payment_methods.sql file

--- macros/get_payment_methods.sql
+++ macros/get_payment_methods.sql
@@ -1,5 +1,4 @@
 {% macro get_payment_methods() %}
-{{ return(["bank_transfer", "credit_card", "gift_card"]) }}
+{{ return(["bank_transfer", "credit_card", "gift_card","UPI","Online Payments", "Installment Payments"]) }}
 {% endmacro %}
-
 
