2d4e51c8c28708e17b383ddd9daf5772c042c6f8
SADIQ SYED <ssd15437@gmail.com>
2026-01-17 10:47:08 +0000

    Added new file get_payment_methods_v3

--- models/order_payment_method_amounts.sql
+++ models/order_payment_method_amounts.sql
@@ -8,18 +8,18 @@
 -- group by 1
 
 
+--["bank_transfer", "credit_card", "gift_card"]
 
+{% set payment_methods =  get_payment_methods2() %}
 
--- {% set payment_methods = ["bank_transfer", "credit_card", "gift_card"] %}
-
--- select
--- order_id,
--- {% for payment_method in payment_methods %}
--- sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
--- {% endfor %}
--- sum(amount) as total_amount
--- from {{ ref('raw_payments') }}
--- group by 1
+select
+order_id,
+{% for payment_method in payment_methods %}
+sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+{% endfor %}
+sum(amount) as total_amount
+from {{ ref('raw_payments') }}
+group by 1
 
 
 
@@ -38,13 +38,13 @@
 
 
 
-{%- set payment_methods = get_payment_methods1() -%}
+-- {%- set payment_methods = get_payment_methods1() -%}
 
-select
-order_id,
-{%- for payment_method in payment_methods %}
-sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount
-{%- if not loop.last %},{% endif -%}
-{% endfor %}
-from {{ ref('raw_payments') }}
-group by 1
+-- select
+-- order_id,
+-- {%- for payment_method in payment_methods %}
+-- sum(case when payment_method = '{{payment_method}}' then amount end) as {{payment_method}}_amount,
+-- {% endfor %}
+-- sum(amount) as total_amount
+-- from {{ ref('raw_payments') }}
+-- group by 1
